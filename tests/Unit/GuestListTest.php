4277228899421537d86a2b110e6141de671bc0d7
danielmen-mx <92502464+danielmen-mx@users.noreply.github.com>
2023-02-26 07:43:04 -0600

    #comment add guest lists tests

--- tests/Unit/GuestListTest.php
+++ tests/Unit/GuestListTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\Guest;
 use App\Models\GuestList;
 use Illuminate\Support\Facades\Log;
 use Tests\TestCase as TestsTestCase;
@@ -19,16 +20,77 @@ class GuestListTest extends TestsTestCase
         parent::setUp();
         GuestList::factory(10)->create();
         $this->setFullName();
-        $this->createGuestList();
     }
 
     /** @test */
     function guest_request_match_with_guest_list()
-    {   
+    {
+        $this->createGuestList();
         $request = $this->createRequest('POST', $this->getNameArray());
         $guestInLists = GuestList::get();
-        // exec the search of match name
-        // assert the one who match
+
+        $guestInLists->each(function ($guestList) use ($request) {
+            $match = $guestList->match($request);
+
+            if ($match == false) return;
+
+            $this->assertComparison($guestList, $request);
+        });
+
+        $this->assertTrue($guestInLists->count() > 1);
+    }
+
+    /** @test */
+    function guest_request_doesnt_match_with_any_guest_list()
+    {        
+        $request = $this->createRequest('POST', $this->getNameArray());
+        $guestInLists = GuestList::get();
+
+        $guestInLists->each(function ($guestList) use ($request) {
+            $match = $guestList->match($request);
+
+            if ($match == false) return;
+
+            $this->assertTrue(false);
+        });
+
+        $this->assertTrue($guestInLists->count() > 1);
+    }
+
+    /** @test */
+    function guest_request_only_have_one_name_and_last_name_match_with_guest_list()
+    {
+        $fullName = [
+            'first_name' => $this->firstName,
+            'second_name' => '',
+            'first_last_name' => $this->firstLastName,
+            'second_last_name' => '',
+        ];
+
+        $guestList = GuestList::factory()->create($fullName);
+        $request = $this->createRequest('POST', $fullName);
+
+        $this->assertTrue($guestList->match($request));
+    }
+
+    /** @test */
+    function guest_list_related_with_guest()
+    {
+        $guest = $this->createGuest();
+        $guestList = $this->createGuestList($guest->id);
+
+        $guestRelation = $guestList->guest;
+        $this->assertComparison($guest, $guestRelation);
+    }
+
+    private function assertComparison($value1, $value2)
+    {        
+        $this->assertTrue(
+            $value1->first_name == $value2->first_name,
+            $value1->second_name == $value2->second_name,
+            $value1->first_last_name == $value2->first_last_name,
+            $value1->second_last_name == $value2->second_last_name
+        );
     }
 
     private function getNameArray()
@@ -49,8 +111,15 @@ class GuestListTest extends TestsTestCase
         $this->secondLastName = $this->createLastNames();
     }
 
-    private function createGuestList()
+    private function createGuest()
     {
-        return GuestList::factory()->create($this->getNameArray());
+        return Guest::factory()->create($this->getNameArray());
+    }
+
+    private function createGuestList($guestListId = null)
+    {        
+        $array = $this->getNameArray();
+        $array['guest_id'] = $guestListId;
+        return GuestList::factory()->create($array);
     }
 }
