816d4b6df00e5bc146d4eda00ba488a6948d72a1
Daniel Mendez <danielmenc@outlook.com>
2023-02-27 14:39:38 -0600

    #comment add file to manage the responses from the Apis

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ApiController extends Controller
+{
+    public function responseWithData($data, $message, $statusCode = 200)
+    {
+        $response = (object) [
+            'message' => $message,
+            'data' => $data,
+        ];
+
+        return response()->json($response, $statusCode);
+    }
+
+    public function responseWithError($exception, $message)
+    {        
+        $response = (object) [
+            'message' => $message,
+            'exception' => $exception ? $exception->getMessage() : null,
+        ];
+
+        $code = $exception ? $exception->getCode() : 417;
+
+        return response()->json($response, $code);
+    }
+}
