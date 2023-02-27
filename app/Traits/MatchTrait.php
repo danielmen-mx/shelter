02c98f2aa4da4a75fdef8504507fcc63c972dbd2
Daniel Mendez <danielmenc@outlook.com>
2023-02-27 14:41:20 -0600

    #comment fix trait to match coincidencies

--- app/Traits/MatchTrait.php
+++ app/Traits/MatchTrait.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 trait MatchTrait {
 
-    public function findMatch(String $model_string, Request $request)
+    public function findMatch(string $model_string, array $request): object
     {
         $model = 'App\\Models\\'.$model_string;
         $collection = $model::get();
@@ -14,15 +14,19 @@ trait MatchTrait {
             return $this->match($item, $request);
         });
 
+        if ($match->count() > 0) {
+            $match = $match->first();
+        }
+
         return $match;
     }
 
-    private function match(object $item, Request $request): bool
-    {                
-        if ($item->first_name != $request->first_name) return false;
-        if ($item->second_name != $request->second_name) return false;
-        if ($item->first_last_name != $request->first_last_name) return false;
-        if ($item->second_last_name != $request->second_last_name) return false;
+    private function match(object $item, array $request): bool
+    {
+        if ($item->first_name != $request['first_name']) return false;
+        if ($item->second_name != $request['second_name']) return false;
+        if ($item->first_last_name != $request['first_last_name']) return false;
+        if ($item->second_last_name != $request['second_last_name']) return false;
 
         return true;
     }
